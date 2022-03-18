70132e13adc10b5001f2d2c93f8b0f12f4838191
devindice <bpapilot@gmail.com>
2022-03-18 04:37:59 +0000

    Fixed function typo

--- app/config.py
+++ app/config.py
@@ -10,7 +10,7 @@ def read():
     except:
         print("Config does not exist, creating")
         config = {'ticksFullRotation':0,'style':'interleave','currentHr':12,'currentMn':0,'resetHr':12,'resetMn':0,'limitRotationHr':0,'limitRotationMn':0,'triggerHr':1,'triggerMn':0,'triggerPin':37,'triggerDelay':3,'triggerDuration':0.25,'movementDelay':0.25,'movemnentRotation':'full','movementSnap':True,'mqttEnable':True,'mqttBroker':'192.168.1.100','mqttTopicIn':'/pi/clock/in','mqttTopicOut':'/pi/clock/out','autoResetSeconds':300,'reduceDrift':True,'mode':'run','calibrateMethod':4,'autoUpdate':True,'buttonHrFwPin':32,'buttonHrRvPin':36,'buttonMnFwPin':38,'buttonMnRvPin':40}
-        configWrite(config)    
+        write(config)    
     return config
 
 def write(config):
