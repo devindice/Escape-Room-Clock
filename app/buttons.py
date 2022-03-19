43b3d7bf5354f972128f1eab74e715ab599a4155
devindice <bpapilot@gmail.com>
2022-03-19 03:45:57 +0000

    Added Buttons

---
+++ app/buttons.py
@@ -0,0 +1,76 @@
+import app.config as config
+import app.clock as clock
+import app.mqtt as mqtt
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import time
+
+settings = config.read()
+buttons = ['buttonHrFw','buttonHrRv','buttonMnFw','buttonMnRv']
+#buttons = ['buttonHrFw']
+
+# Set pin numbers
+buttonHrFwPin = settings.get('buttonHrFwPin')
+buttonHrRvPin = settings.get('buttonHrRvPin')
+buttonMnFwPin = settings.get('buttonMnFwPin')
+buttonMnRvPin = settings.get('buttonMnRvPin')
+
+# Set button type (Normally Open or Normally Closed)
+buttonHrFwType = settings.get('buttonHrFwType')
+buttonHrRvType = settings.get('buttonHrRvType')
+buttonMnFwType = settings.get('buttonMnFwType')
+buttonMnRvType = settings.get('buttonMnRvType')
+
+def setPins():
+    GPIO.setwarnings(False) # Ignore warning for now
+    GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+
+    for button in buttons:
+        pin = settings.get(button + 'Pin')
+        # Use 3v rail for buttons to pins
+        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+
+def check():
+    for button in buttons:
+        bPin = settings.get(button + 'Pin')
+        bType = settings.get(button + 'Type')
+        if bType == 'normClosed':
+            if GPIO.input(bPin) == GPIO.LOW:
+                print("Button %s was pushed!" %(button))
+                if button == 'buttonHrFw':
+                    print('adding hour')
+                    clock.time('add', 'hour')
+                elif button == 'buttonHrRv':
+                    print('subtracting hour')
+                    clock.time('subtract', 'hour')
+                elif button == 'buttonMnFw':
+                    print('Adding minute')
+                    clock.time('add', 'minute')
+                elif button == 'buttonMnRv':
+                    print('Subtracting minute')
+                    clock.time('subtract', 'minute')
+                mqtt.publish()
+                while GPIO.input(bPin) == GPIO.LOW:
+                    time.sleep(0.01)
+        elif bType == 'normOpen':
+            if GPIO.input(bPin) == GPIO.HIGH:
+                print("Button %s was pushed!" %(button))
+                if button == 'buttonHrFw':
+                    print('adding hour') 
+                    clock.time('add', 'hour')
+                elif button == 'buttonHrRv':
+                    print('subtracting hour')
+                    clock.time('subtract', 'hour')
+                elif button == 'buttonMnFw':
+                    print('Adding minute')
+                    clock.time('add', 'minute')
+                elif button == 'buttonMnRv':
+                    print('Subtracting minute')
+                    clock.time('subtract', 'minute')
+                mqtt.publish()
+                while GPIO.input(bPin) == GPIO.HIGH:
+                    time.sleep(0.01)
+        else:
+            print('Unknown button type')
+            time.sleep(1)
+
+
