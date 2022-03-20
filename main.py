698f4351c46acdee5e9c5cf568cd545f7487e8e0
devindice <bpapilot@gmail.com>
2022-03-20 05:08:45 +0000

    Cleaned up main

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import app.config as config
 import app.mqtt as mqtt
 import app.buttons as buttons
-import time
 
-settings = config.read()
 backgroundTask1 = mqtt.listener()
 backgroundTask2 = buttons.listener()
 
-#buttons.setPins()
-#while True:
-#    buttons.check()
-#    time.sleep(0.01)
