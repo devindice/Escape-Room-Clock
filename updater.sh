608ca8c98cdc407a2a315171eccbcbbda3a2d776
Devin Dice <bpapilot@gmail.com>
2022-03-27 16:48:48 -0400

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -4,10 +4,10 @@ git stash &&
 git clean -fd &&
 git pull &&
 for FILE in $(ls -1a | tail -n+3 | grep -v 'scheduler.cron\|logrotate.conf');do
-    chown -R pi:pi $FILE;done &&
-    chown root:root /home/pi/Escape-Room-Clock/scheduler.cron;
-    chown root:root /home/pi/Escape-Room-Clock/logrotate.conf;
-    chmod 644 /home/pi/Escape-Room-Clock/scheduler.cron;
-    chmod 644 /home/pi/Escape-Room-Clock/logrotate.conf;
+    chown -R pi:pi $FILE;
 done &&
+chown root:root /home/pi/Escape-Room-Clock/scheduler.cron &&
+chown root:root /home/pi/Escape-Room-Clock/logrotate.conf &&
+chmod 644 /home/pi/Escape-Room-Clock/scheduler.cron &&
+chmod 644 /home/pi/Escape-Room-Clock/logrotate.conf &&
 kill $(ps aux | grep python | grep main.py | grep -v grep | awk '{print $2}')
