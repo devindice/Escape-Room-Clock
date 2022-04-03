162377dc30d543e31d7a58a05c41760b78672773
Devin Dice <bpapilot@gmail.com>
2022-04-03 13:26:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ import app.buttons as buttons
 import app.motor as motor
 import app.control as control
 import RPi.GPIO as gpio 
+import time
 
+logger.log.info('Python Started. Waiting 1 minute for other services to start.')
+time.sleep(60)
 
 # Import parameters from disk
 parameters = config.parameters
@@ -22,4 +25,3 @@ Service2 = buttons.service(parameters)
 Service3 = config.service(parameters)
 Service4 = motor.service(parameters)
 Service5 = control.service(parameters)
-
